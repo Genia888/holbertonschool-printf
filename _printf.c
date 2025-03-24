f133f6f9d4142a5943a31fc3d998812e190c6468
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-24 17:43:16 +0000

    first version for testing compiling

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _printf - display variadic function printf
+ * @format: format type like printf
+ *
+ * Return: length of the string
+ */
+int _printf(const char *format, ...)
+{
+	(void)format;
+	_putchar('X');
+	return 1;
+}
