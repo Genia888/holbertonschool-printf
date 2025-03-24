7df57951c29dcd887032f5fc83bb5ce66fa93251
SebSa12000 <sebastien.salgues@gmail.com>
2025-03-24 18:25:39 +0000

    Structuration of files without code

--- _printf.c
+++ _printf.c
@@ -7,6 +7,15 @@
  */
 int _printf(const char *format, ...)
 {
+	to_do_struct ops[] = {
+		{"%", to_do_from_pourcent},
+		{"c", to_do_from_char},
+		{"i", to_do_from_int},
+		{"s", to_do_from_string},
+		{"d", to_do_from_int},
+		{NULL, NULL}
+	};
+	(void)ops;
 	(void)format;
 	_putchar('X');
 	return 1;
---
+++ to_do_from_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * to_do_from_string - get an argument from string and display it
+ *
+ * Return: Length of the string display
+ */
+int to_do_from_string(va_list va)
+{
+	(void)va;
+	return 1;
+}
